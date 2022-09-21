ce088866a56c71d6f3b7398ada1a3dfba6d4a255
carlosalesilva <carlosalexandresilva100@gmail.com>
2022-09-21 20:31:07 -0300

    Construindo tabela de exibição.

---
+++ src/data/dados.ts
@@ -0,0 +1,6 @@
+const dados = [
+  {id:1, titulo:'trabalho', descricao:'trabalho em equipe', conteudo:'ajkdshflhadfkhashkfhjdashlkh'},
+  {id:2, titulo:'apresentação', descricao:'disciplina de web', conteudo:'dia 26/09/2022'}
+]
+
+export default dados;
--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,9 +1,48 @@
- import React from 'react';
- 
- // import { Container } from './styles';
- 
- const Task: React.FC = () => {
-   return <h1>Task page</h1>;
- }
- 
- export default Task;
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import Table from 'react-bootstrap/esm/Table';
+import dados from '../../data/dados'
+
+
+
+function getLinhas() {
+  return dados.map(dados =>{
+    return(
+      <tr>
+        <td>{dados.id}</td>
+        <td>{dados.titulo}</td>
+        <td>{dados.descricao}</td>
+        <td>
+          <Button size='sm'>Editar</Button>{' '}
+          <Button size='sm' variant="info">Visualizar</Button>{' '}
+          <Button size='sm' variant="danger">Remover</Button>{' '}
+        </td>
+      </tr>
+    )
+  })
+}
+
+const Task: React.FC = () => {
+  return (
+    <div className='container'>
+      <br />
+      <h1>Task page</h1>
+      <br />
+      <Table striped bordered hover className='text-center'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Título</th>
+            <th>Descrição</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+        {getLinhas()}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+export default Task;
